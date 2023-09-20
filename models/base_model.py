328275453b5ed31fae0f99e6f687df313f911785
Tutrue <hamdy.xd55@gmail.com>
2023-09-20 20:08:14 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -44,12 +44,11 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        dictionary.pop("_sa_instance_state", None)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
         return dictionary
     
     def delete(self):
