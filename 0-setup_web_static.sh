2d5a6d61a82577d5a375e63b002b84c970da0f9e
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-05 16:54:16 +0300

    script that sets up my web servers for deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# sets up my web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+
+sudo sh -c 'echo "Hello form TutTrue >_<" >/data/web_static/releases/test/index.html'
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+# add config file
+sudo sh -c 'echo "\
+server {
+    listen 80;
+    add_header X_Served_by \$HOSTNAME;
+    listen [::]:80 default_server;
+    server_name tuttrue.tech;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}" > /etc/nginx/sites-available/default'
+
+sudo service nginx restart
